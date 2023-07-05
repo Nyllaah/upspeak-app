b76ee30c4647e432d775ed4a238525be6900f068
Nyllaah <nylla.g@hotmail.com>
2023-07-05 18:13:14 -0300

    feat: add the selected class to the switch button on html

--- js/sign-in-screen.js
+++ js/sign-in-screen.js
@@ -1,16 +1,7 @@
 const logInBtn = document.querySelector('#login-btn');
 const signInBtn = document.querySelector('#sign-in-btn');
 
-function switchBtn() {
-    if (signInBtn.style.backgroundColor = '#A134F6') {
-        signInBtn.style.backgroundColor = '#FFF';
-        signInBtn.style.color = '#F5C620';
-    } else if (signInBtn.style.backgroundColor = '#FFF') {
-        signInBtn.style.backgroundColor = '#A134F6';
-        signInBtn.style.color = '#F5C620';
-    }
-}
-
-window.onload = () => {
-    signInBtn.addEventListener('click', switchBtn);
-}
+signInBtn.addEventListener('click', (e) => {
+    e.target.classList.add('selected')
+    if (e.target.style.backgroundColor = white)
+})
