7d018d22b30d09784bdbc6813ed365aeec368aa2
Nyllaah <nylla.g@hotmail.com>
2023-07-05 18:02:00 -0300

    feat: fixed title position

--- js/sign-in-screen.js
+++ js/sign-in-screen.js
@@ -2,7 +2,7 @@ const logInBtn = document.querySelector('#login-btn');
 const signInBtn = document.querySelector('#sign-in-btn');
 
 function switchBtn() {
-    if (signInBtn.style.color === '#FFF') {
+    if (signInBtn.style.backgroundColor = '#A134F6') {
         signInBtn.style.backgroundColor = '#FFF';
         signInBtn.style.color = '#F5C620';
     } else {
