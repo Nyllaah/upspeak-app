a3131a5c3d89a03bc62503a80120399230531bfc
Nyllaah <nylla.g@hotmail.com>
2023-07-05 21:52:58 -0300

    feat: screen switch function working

--- js/sign-in-screen.js
+++ js/sign-in-screen.js
@@ -1,27 +1,34 @@
-function switchBtnColor() {
-    const switches = document.querySelectorAll('.switch');
+function switchScreens() {
+    const switches = document.querySelectorAll('.switch')
     for (let i = 0; i < switches.length; i++) {
-        switches[i].addEventListener('click', (e) => {
-            const selected = document.querySelector('.selected');
-            if (selected) {
-                selected.classList.remove('selected');
+        switches[i].addEventListener('click', (ev) => {
+
+            let selected = document.querySelector('.selected');
+            selected.classList.remove('selected');
+            ev.target.classList.add('selected');
+            selected = document.querySelector('.selected');
+            
+            const logInBtn = document.querySelector('#login-btn');
+            const inputs = document.querySelectorAll('.input');
+
+            if (ev.target === logInBtn) {
+                for (let i = 0; i < inputs.length; i++) {
+                    if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
+                        inputs[i].style.display = 'none';
+                        inputs[i].firstElementChild.required = false;
+                    }
+                }
+            } else {
+                for (let i = 0; i < inputs.length; i++) {
+                    if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
+                        inputs[i].style.display = '';
+                        inputs[i].firstElementChild.required = true;
+                    }
+                }
             }
-            e.target.classList.add('selected'); 
         })
     }
-    e.target.className.add('selected');
-}
-
-function hideInputs() {
-    
-}
-
-function switchScreens() {
-    switchBtnColor();
-    
-
 }
 
 switchScreens();
-
 
