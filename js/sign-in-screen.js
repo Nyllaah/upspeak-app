344a47a49fb64c0537c987da2b0681243e728f03
Nyllaah <nylla.g@hotmail.com>
2023-07-06 11:45:43 -0300

    feat: refactoring switchScreen function

--- js/sign-in-screen.js
+++ js/sign-in-screen.js
@@ -1,33 +1,39 @@
-function switchScreens() {
-    const switches = document.querySelectorAll('.switch')
-    for (let i = 0; i < switches.length; i++) {
-        switches[i].addEventListener('click', (ev) => {
+const switches = document.querySelectorAll('.switch');
+const logInBtn = document.querySelector('#login-btn');
+const inputs = document.querySelectorAll('.input');
 
-            let selected = document.querySelector('.selected');
-            selected.classList.remove('selected');
-            ev.target.classList.add('selected');
-            selected = document.querySelector('.selected');
-            
-            const logInBtn = document.querySelector('#login-btn');
-            const inputs = document.querySelectorAll('.input');
+function selectScreen(ev) {
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  ev.target.classList.add('selected');
+  selected = document.querySelector('.selected');
+}
 
-            if (ev.target === logInBtn) {
-                for (let i = 0; i < inputs.length; i++) {
-                    if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
-                        inputs[i].style.display = 'none';
-                        inputs[i].firstElementChild.required = false;
-                    }
-                }
-            } else {
-                for (let i = 0; i < inputs.length; i++) {
-                    if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
-                        inputs[i].style.display = '';
-                        inputs[i].firstElementChild.required = true;
-                    }
-                }
-            }
-        })
+function hideInputs(ev) {
+  if (ev.target === logInBtn) {
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
+        inputs[i].style.display = 'none';
+        inputs[i].firstElementChild.required = false;
+      }
     }
+    } else {
+      for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].firstElementChild.id !== 'email-input' && inputs[i].firstElementChild.id !== 'password-input') {
+          inputs[i].style.display = '';
+          inputs[i].firstElementChild.required = true;
+        }
+      }
+    }
+}
+
+function switchScreens() {
+  for (let i = 0; i < switches.length; i++) {
+    switches[i].addEventListener('click', (ev) => {
+      selectScreen(ev);
+      hideInputs(ev);
+    })
+  }
 }
 
 switchScreens();
